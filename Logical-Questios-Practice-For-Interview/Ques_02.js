ed0fff8c42e660f583e64fc60826b5e38b9d89d5
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-05-15 06:27:42 +0500

    same question with different logic of for-of loop

--- Logical-Questios-Practice-For-Interview/Ques_02.js
+++ Logical-Questios-Practice-For-Interview/Ques_02.js
@@ -1,19 +1,38 @@
-// 🔄 Question 2: Reverse a String
-// 👉 Problem Statement:
+// // 🔄 Question 2: Reverse a String
+// // 👉 Problem Statement:
 
-// Write a function in JavaScript that takes a string as input and returns the reversed version of that string.
+// // Write a function in JavaScript that takes a string as input and returns the reversed version of that string.
 
-const reverseString = (str) => {
-    const splitted_str = str.split("")
+// const reverseString = (str) => {
+//     const splitted_str = str.split("")
     
-    let reversed_str = []
+//     let reversed_str = []
 
-    for (let i = splitted_str.length - 1 ; i >= 0 ; i--) {
-        reversed_str.push(splitted_str[i])
+//     for (let i = splitted_str.length - 1 ; i >= 0 ; i--) {
+//         reversed_str.push(splitted_str[i])
+//     }
+
+//     return reversed_str.join("")
+
+// }
+
+// console.log(reverseString("Owais"))
+
+// -------------------
+
+// Solve this question using for-of loop now: 
+
+const reverseStr = (str) => {
+
+    let reversed_str = ""
+
+
+    for (let char of str) {
+        reversed_str = char + reversed_str
     }
 
-    return reversed_str.join("")
+    return reversed_str
 
-}
+} 
 
-console.log(reverseString("Owais"))
+console.log(reverseStr("Owais"))
