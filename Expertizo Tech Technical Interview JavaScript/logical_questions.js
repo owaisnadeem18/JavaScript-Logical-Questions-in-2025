eb95702d52874a9e6abaf437c0ae0da492512a0f
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2025-05-21 16:57:16 +0500

    added question 02 of technical interview

--- Expertizo Tech Technical Interview JavaScript/logical_questions.js
+++ Expertizo Tech Technical Interview JavaScript/logical_questions.js
@@ -1,8 +1,8 @@
 const students = [
-  {id: 101,name: "Ali Raza",course: "Web Development",semester: "5th"},
-  {id: 102,name: "Sara Khan",course: "Mobile App Development",semester: "4th"},
+  {id: 101,name: "ali Raza",course: "Web Development",semester: "5th"},
+  {id: 102,name: "sara Khan",course: "Mobile App Development",semester: "4th"},
   {id: 103,name: "Usman Ahmed",course: "Data Science",semester: "6th"},
-  {id: 104,name: "Hira Sheikh",course: "Artificial Intelligence",semester: "7th"},
+  {id: 104,name: "hira Sheikh",course: "Artificial Intelligence",semester: "7th"},
   {id: 105,name: "Bilal Aslam",course: "Cyber Security",semester: "3rd"}
 ];
 
@@ -21,6 +21,14 @@ const students = [
 
 // More Shorter Way: 
 
-let PushIntoArray = AllStudents => AllStudents.map(obj => obj.name)
+// let PushIntoArray = AllStudents => AllStudents.map(obj => obj.name)
 
-console.log(PushIntoArray(students))
+// console.log(PushIntoArray(students))
+
+// ----------------------------- Question 02 ------------------------
+
+const capitalizeName = (studentsArr) => {
+  return studentsArr.map((obj) => obj.name[0].toUpperCase() + obj.name.slice(1))
+}
+
+console.log(capitalizeName(students))
