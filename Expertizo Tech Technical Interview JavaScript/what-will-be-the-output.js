da230f338b74a87e35ca840c6018ba40ec1d109b
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2025-05-23 19:49:08 +0500

    added some tricky questions

---
+++ Expertizo Tech Technical Interview JavaScript/what-will-be-the-output.js
@@ -0,0 +1,15 @@
+// 1. --------------------------------
+
+// let a = [1, 2, 3];
+// let b = [1, 2, 3];
+
+// console.log(a == b);
+
+// Output => false (Because the memory refrence would not be )
+
+// 2. --------------------------------
+
+let x = [1, 2, 3];
+let y = x;
+
+console.log(x == y); // true
