a8366fb1f222be16567739361d7eecf3d394e112
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-06-03 23:54:35 +0500

    print typeof undefined

--- premitive-datatypes/output.js
+++ premitive-datatypes/output.js
@@ -2,4 +2,8 @@
 
 // console.log(typeof string)
 
-console.log(typeof null) // object
+// console.log(typeof null) // object
+
+// console.log(typeof NaN) // Number
+
+console.log(typeof undefined) // undefined
