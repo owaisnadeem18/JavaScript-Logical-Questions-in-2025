87e4285d61bf0119efefaf52b19d33ef875e1134
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-06-08 23:37:12 +0500

    setTimeOut Technical Question

---
+++ Advanced_Technical-Questions.js
@@ -0,0 +1,17 @@
+for (var i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 1000);
+}
+
+// My Output: 
+// 0
+// 1
+// 2 
+
+// Correct Answer: 
+
+// 3 
+// 3 
+// 3 
+
+// Explanation: Because setTimeOut is an async function , it will run after 1000 mil sec and the loop will turn it's value to 3 and will print 3 in each call.
+
