a5b36f043728e89299b5c959ff89c6e9ddf4a5a4
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-06-08 23:40:16 +0500

    calling the function in loop and guess answer

--- Advanced_Technical-Questions.js
+++ Advanced_Technical-Questions.js
@@ -1,6 +1,6 @@
-for (var i = 0; i < 3; i++) {
-  setTimeout(() => console.log(i), 1000);
-}
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1000);
+// }
 
 // My Output: 
 // 0
@@ -14,4 +14,20 @@ for (var i = 0; i < 3; i++) {
 // 3 
 
 // Explanation: Because setTimeOut is an async function , it will run after 1000 mil sec and the loop will turn it's value to 3 and will print 3 in each call.
+
+for (var i = 0; i < 3; i++) {
+  function log() {
+    console.log(i);
+  }
+  log();
+}
+
+// My Output: 
+
+// 0 
+// 1 
+// 2
+
+// Above is Corrected Answer Output 
+
 
