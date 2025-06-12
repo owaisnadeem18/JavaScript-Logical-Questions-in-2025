3b760dff7b6e9abc4e787f1c6c28dbae98e14772
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-06-12 23:48:35 +0500

    sort array and find second highest num with built in method

---
+++ Question-13.js
@@ -0,0 +1,13 @@
+// Find the second highest number in an array without any built in method 
+
+let arr = [34, 24 ,52 ,13 ,64 , 34]
+
+let FindSecondHighestNum = (array) => {
+
+    let sorted_Arr = array.sort((a,b) => a-b)
+
+    return sorted_Arr[array.length-2]
+
+}
+
+console.log(`The highest number in an array is => ${FindSecondHighestNum(arr)}`)
