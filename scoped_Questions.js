ab11efeff76adc04b8ad5d3da677be6f88969cbc
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2025-06-19 12:29:20 +0500

    Var , let and const questions with scoping & block

---
+++ scoped_Questions.js
@@ -0,0 +1,44 @@
+// var a = 35 
+
+// {
+//     var a = 45
+//     console.log(a)
+// }
+
+// const updateVariable = (varaibleValue) => varaibleValue = 34
+
+// console.log(a)
+// console.log(updateVariable(a))
+// console.log(a)
+
+// // 2.
+
+// function test() {
+//   console.log( "The value of foo is => " , foo);
+//   let foo = 'Hello';
+//   console.log(foo);
+// }
+
+// test();
+
+// 3.
+
+// console.log(f)
+// var f = 45 
+
+// ✅ 1. ReferenceError
+// Kab aata hai? Jab koi variable ya function exist hi nahi karta aur tum usko access karne ki koshish karte ho.
+
+// console.log(r) // Not Defined -> Even doesn't exist and JavaScript don't knows about it (ReferenceError: x is not defined)
+
+// console.log(a) // TDZ (Refrence error , only value hoisted but not initialized)
+// let a = 45
+
+// let a =40
+
+// {
+//     let a = 45
+//     console.log(a)
+// }
+
+// console.log(a)
