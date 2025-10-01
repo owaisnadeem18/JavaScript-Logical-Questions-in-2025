951a675a04ed98cd89306268b0193a557ad3759f
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2025-10-01 23:46:30 +0500

    Added variable scoped question

--- What-will-be-the-output.js
+++ What-will-be-the-output.js
@@ -1,13 +1,36 @@
-// Function Hoisting: 
+// ***************************************************** // Function Hoisting: *****************************************************  
 
-foo();
-bar();
+// ----------------------------------------------------------------------------------- 
 
-function foo() {
-  console.log("foo called");
-}
+// foo();
+// bar();
 
-var bar = function() {
-  console.log("bar called");
-}
+// function foo() {
+//     console.log("foo called");
+// }
 
+// var bar = function() {
+//     console.log("bar called");
+// }
+
+// output: 
+// foo called 
+// TypeError: bar is not a function
+
+// Reason:
+// foo(); works → function declaration hoisted
+// bar(); ❌ fails → only variable hoisted, not function body
+
+// ----------------------------------------------------------------------------------- 
+
+// Scope of variables:
+
+console.log(x);
+var x = 5; // undefined
+
+
+console.log(y); // Refrence Error , nor the variable nor the value hoisted in "let"
+let y = 10;
+
+console.log(z); // Refrence Error , nor the variable nor the value hoisted in "const" 
+const z = 20;
