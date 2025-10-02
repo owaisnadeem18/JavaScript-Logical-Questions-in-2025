2b106695d8566aee1b090a5ee4c5c32fad8fad8b
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2025-10-02 16:14:48 +0500

    type of numbers

--- What-will-be-the-output.js
+++ What-will-be-the-output.js
@@ -25,12 +25,17 @@
 
 // Scope of variables:
 
-console.log(x);
-var x = 5; // undefined
+// console.log(x);
+// var x = 5; // undefined
 
 
-console.log(y); // Refrence Error , nor the variable nor the value hoisted in "let"
-let y = 10;
+// console.log(y); // Refrence Error , nor the variable nor the value hoisted in "let"
+// let y = 10;
 
-console.log(z); // Refrence Error , nor the variable nor the value hoisted in "const" 
-const z = 20;
+// console.log(z); // Refrence Error , nor the variable nor the value hoisted in "const" 
+// const z = 20;
+
+// ----------------------------------------------------------------------------------- 
+
+console.log(typeof NaN);
+console.log(NaN === NaN);
