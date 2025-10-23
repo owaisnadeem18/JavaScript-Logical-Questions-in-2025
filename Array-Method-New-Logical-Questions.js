905d54ecad30ddef4f31b42de664c49c20beb45f
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2025-10-24 00:06:42 +0500

    logical question

--- Array-Method-New-Logical-Questions.js
+++ Array-Method-New-Logical-Questions.js
@@ -27,6 +27,24 @@ const arr = [4 , 67 , 23 , 24 ,21 ,778 ,24 , 213 ,12 , 12 ,4 ,67 ,24 , 213]
 // console.log(findLargetElemInArr(arr))
 
 // Reverse an array without using .reverse().
+
+const reverseAnArray = (array) => {
+    let sorted_arr = array.sort((a,b) => a-b)
+
+    let arr_removed_duplicates = [...new Set(sorted_arr)]
+
+    let reversed_arr = []
+
+    for (let i = arr_removed_duplicates.length - 1 ; i >= 0 ; i--) {
+        reversed_arr.push(arr_removed_duplicates[i])
+    }
+
+    return reversed_arr
+
+}
+
+console.log(reverseAnArray(arr))
+
 // Count how many times each element appears in an array.
 // Merge two arrays and remove duplicates.
 // Rotate an array k times to the right.
