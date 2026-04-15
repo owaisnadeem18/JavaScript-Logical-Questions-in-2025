7af58e10408c526d1556fa8505263e822882669f
owaisnadeem18 <owaisnadeem15@gmail.com>
2026-04-15 16:44:19 +0500

    non repeating array elements

---
+++ eOcean Logical JS questions Preparation/index.js
@@ -0,0 +1,40 @@
+// 🔥 1. Array / Object Logic (VERY IMPORTANT):
+
+// ✅ Q1: Find first non-repeating element:
+
+// [4,5,1,2,0,4]
+
+const array = [4,5,1,2,0,4]
+
+const firstNonRepeating = (a) => {
+
+    let counts = {}
+
+    for (let r = 0 ; r < a.length ; r++) {
+        let element = a[r]
+
+        if (counts[element]) {
+            counts[element]++
+        }
+
+        else {
+            counts[element] = 1
+        }
+
+    }
+
+    for (let i = 0 ; i < a.length ; i++) {
+        let element = a[i]
+
+        if (counts[element] === 1) {
+            return element
+        }
+
+    }
+
+}
+
+console.log(firstNonRepeating(array))
+
+
+// Expected Output: 5
