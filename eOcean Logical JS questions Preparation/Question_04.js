1f71af77a698fb12fe355d753a76632f130b1c92
owaisnadeem18 <owaisnadeem15@gmail.com>
2026-04-17 03:42:24 +0500

    group anagram question

--- eOcean Logical JS questions Preparation/Question_04.js
+++ eOcean Logical JS questions Preparation/Question_04.js
@@ -3,6 +3,34 @@
 
 // Input: ["eat", "tea", "tan", "ate", "nat", "bat"]
 
+const arr = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+const groupAnagrams = (arr) => {
+
+    let obj = {}
+    
+
+    for (let i = 0 ; i< arr.length ; i++) {
+        
+        let word = arr[i] // "eat"
+
+        let key = arr[i].split("").sort().join("") // "aet"
+
+        if (obj[key]) { // condition fails in the first iteration
+            obj[key].push(word)
+        }
+        else {
+            obj[key] = [word] 
+        }
+
+    }
+
+    return Object.values(obj)
+
+}
+
+console.log(groupAnagrams(arr))
+
 
 
 // Output:
