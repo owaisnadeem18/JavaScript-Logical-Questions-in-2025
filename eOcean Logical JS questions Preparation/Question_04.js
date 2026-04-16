e031808d3a40ab89c1c2864166c80fbc004cfada
owaisnadeem18 <owaisnadeem15@gmail.com>
2026-04-17 03:01:04 +0500

    anagram question revision

---
+++ eOcean Logical JS questions Preparation/Question_04.js
@@ -0,0 +1,17 @@
+// Question #04 (VERY IMPORTANT - Asked a lot)
+// 🔍 Group Anagrams
+
+// Input: ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+
+
+// Output:
+// [
+//   ["eat","tea","ate"],
+//   ["tan","nat"],
+//   ["bat"]
+// ]
+
+// 👉 Concept:
+
+// Sorting + HashMap
--- eOcean Logical JS questions Preparation/logicalQuestions.js
+++ eOcean Logical JS questions Preparation/logicalQuestions.js
@@ -1,25 +1,48 @@
-const array = [4,5,1,2,0,4]
+// const array = [4,5,1,2,0,4]
 
-const firstNonRepeatingElement = (arr) => {
+// const firstNonRepeatingElement = (arr) => {
 
-    const count = {}
+//     const count = {}
 
-    for (let i = 0 ; i < arr.length ; i++) {
+//     for (let i = 0 ; i < arr.length ; i++) {
         
-        if (count[arr[i]]) {
-            count[arr[i]]++
-        }
-        else {
-            count[arr[i]] = 1
-        }
+//         if (count[arr[i]]) {
+//             count[arr[i]]++
+//         }
+//         else {
+//             count[arr[i]] = 1
+//         }
 
-    }
+//     }
 
-    for (let i = 0 ; i < arr.length ; i++) {
-        if (count[arr[i]] === 1) return arr[i]
+//     for (let i = 0 ; i < arr.length ; i++) {
+//         if (count[arr[i]] === 1) return arr[i]
+//     }
+
+// }
+
+
+// console.log(firstNonRepeatingElement(array))
+
+// angram checker: 
+
+function isAnagram (str1 , str2) {
+    
+    if (str1.length == str2.length) {
+        
+    let sorted_str_1 = str1.split("").sort().join("")
+    let sorted_str_2 = str2.split("").sort().join("")
+
+        return sorted_str_1
+
+    if (sorted_str_1 == sorted_str_2) {
+        return "Anagrams"
     }
 
 }
 
+return "Non Anagrams ! "
+    
+}
 
-console.log(firstNonRepeatingElement(array))
+console.log(isAnagram("listen" , "silent"))
