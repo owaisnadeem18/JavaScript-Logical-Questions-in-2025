6afe6b77be55236920d30752f29eb70f6e12f248
owaisnadeem18 <owaisnadeem15@gmail.com>
2026-04-16 23:32:41 +0500

    revision of non repeating elements

---
+++ eOcean Logical JS questions Preparation/logicalQuestions.js
@@ -0,0 +1,27 @@
+const array = [4,5,1,2,0,4]
+
+const firstNonRepeatingElement = (arr) => {
+
+    const count = {}
+
+    for (let i = 0 ; i < arr.length ; i++) {
+        
+        if (count[arr[i]]) {
+            count[arr[i]]++
+        }
+        else {
+            count[arr[i]] = 1
+        }
+
+    }
+
+    for (let i = 0 ; i < arr.length ; i++) {
+        if (count[arr[i]] === 1) return arr[i]
+    }
+
+
+
+}
+
+
+console.log(firstNonRepeatingElement(array))
