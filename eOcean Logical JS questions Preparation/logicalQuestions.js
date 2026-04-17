e406b75a0e75e1f3baefe2a1f2fb57bbb5659191
owaisnadeem18 <owaisnadeem15@gmail.com>
2026-04-17 13:05:42 +0500

    swapping of numbers

--- eOcean Logical JS questions Preparation/logicalQuestions.js
+++ eOcean Logical JS questions Preparation/logicalQuestions.js
@@ -26,23 +26,47 @@
 
 // angram checker: 
 
-function isAnagram (str1 , str2) {
+// function isAnagram (str1 , str2) {
     
-    if (str1.length == str2.length) {
+//     if (str1.length == str2.length) {
         
-    let sorted_str_1 = str1.split("").sort().join("")
-    let sorted_str_2 = str2.split("").sort().join("")
+//     let sorted_str_1 = str1.split("").sort().join("")
+//     let sorted_str_2 = str2.split("").sort().join("")
 
-        return sorted_str_1
+//         return sorted_str_1
 
-    if (sorted_str_1 == sorted_str_2) {
-        return "Anagrams"
-    }
+//     if (sorted_str_1 == sorted_str_2) {
+//         return "Anagrams"
+//     }
 
-}
+// }
 
-return "Non Anagrams ! "
+// return "Non Anagrams ! "
     
+// }
+
+// console.log(isAnagram("listen" , "silent"))
+
+// Swap Numbers , without creating a new variable: 
+
+let a = 34
+let b = 89
+
+console.log(`Value of {a} before swap is -> ${a}`)
+console.log(`Value of {b} before swap is -> ${b}`)
+
+const swapNumbers = (num1 , num2) => {
+
+    a = a + b
+
+    b = a - b
+
+    console.log(`Value of b after swap is -> ${b}`)
+    
+    a = a - b
+    
+    console.log(`Value of a after swap is -> ${a}`)
+
 }
 
-console.log(isAnagram("listen" , "silent"))
+console.log(swapNumbers(a , b))
