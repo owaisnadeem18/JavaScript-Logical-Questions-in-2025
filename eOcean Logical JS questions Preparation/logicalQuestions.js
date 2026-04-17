42a25e1199b9e397e368d279363ea33d6ae94e06
owaisnadeem18 <owaisnadeem15@gmail.com>
2026-04-17 15:11:23 +0500

    logical questions

--- eOcean Logical JS questions Preparation/logicalQuestions.js
+++ eOcean Logical JS questions Preparation/logicalQuestions.js
@@ -49,24 +49,44 @@
 
 // Swap Numbers , without creating a new variable: 
 
-let a = 34
-let b = 89
+// let a = 34
+// let b = 89
 
-console.log(`Value of {a} before swap is -> ${a}`)
-console.log(`Value of {b} before swap is -> ${b}`)
+// console.log(`Value of {a} before swap is -> ${a}`)
+// console.log(`Value of {b} before swap is -> ${b}`)
 
-const swapNumbers = (num1 , num2) => {
+// const swapNumbers = (num1 , num2) => {
 
-    a = a + b
+//     a = a + b
 
-    b = a - b
+//     b = a - b
 
-    console.log(`Value of b after swap is -> ${b}`)
+//     console.log(`Value of b after swap is -> ${b}`)
     
-    a = a - b
+//     a = a - b
     
-    console.log(`Value of a after swap is -> ${a}`)
+//     console.log(`Value of a after swap is -> ${a}`)
+
+// }
+
+// console.log(swapNumbers(a , b))
+
+const students = [
+  {id: 101,name: "ali Raza",course: "Web Development",semester: "5th"},
+  {id: 102,name: "sara Khan",course: "Mobile App Development",semester: "4th"},
+  {id: 103,name: "Usman Ahmed",course: "Data Science",semester: "6th"},
+  {id: 104,name: "hira Sheikh",course: "Artificial Intelligence",semester: "7th"},
+  {id: 105,name: "Bilal Aslam",course: "Cyber Security",semester: "3rd"}
+];
+
+// separate the name of all students in a new array: 
+
+const capitalizeName = (studentsArr) => {
+
+    let students = studentsArr.map(obj => obj.name[0].toUpperCase() + obj.name.slice(1) )
+
+    return students
 
 }
 
-console.log(swapNumbers(a , b))
+console.log(capitalizeName(students))
