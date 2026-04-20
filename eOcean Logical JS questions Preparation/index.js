84969583df0820ee8e97d5138edb0c3681c63f5c
owaisnadeem18 <owaisnadeem15@gmail.com>
2026-04-20 15:10:48 +0500

    logical output traps

--- eOcean Logical JS questions Preparation/index.js
+++ eOcean Logical JS questions Preparation/index.js
@@ -4,35 +4,63 @@
 
 // [4,5,1,2,0,4]
 
-const array = [4,5,1,2,0,4]
+// const array = [4,5,1,2,0,4]
 
-const firstNonRepeatingElement = (arr) => {
+// const firstNonRepeatingElement = (arr) => {
 
-    let count = {}
+//     let count = {}
 
-    for (let i = 0 ; i < arr.length ; i++) {
+//     for (let i = 0 ; i < arr.length ; i++) {
 
-        if (count[arr[i]]) {
-            count[arr[i]]++
-        } else {
-            count[arr[i]] = 1
-        }
+//         if (count[arr[i]]) {
+//             count[arr[i]]++
+//         } else {
+//             count[arr[i]] = 1
+//         }
 
-    }
+//     }
 
-    for (let i = 0 ; i < arr.length ; i++) {
-        let element = arr[i]
+//     for (let i = 0 ; i < arr.length ; i++) {
+//         let element = arr[i]
 
-        if (count[element] === 1) {
-            return element
-        }
+//         if (count[element] === 1) {
+//             return element
+//         }
 
-    }
+//     }
 
-    return -1
+//     return -1
 
-}
+// }
 
-console.log(firstNonRepeatingElement(array))
+// console.log(firstNonRepeatingElement(array))
 
 // Expected Output: 5
+
+// ********** (Output Based Traps) What will be the output of the following: **********
+
+// let a = [1, 2, 3];
+// let b = [...a];
+
+// console.log(a == b) // Output: false because both of them are stored at different memory locations
+
+// let a = [1, 2, 3]
+
+// let b = a
+
+// console.log(b == a) // Output: true because both of them are pointing at the same memory location.
+
+// let a = [1, 2, 3];
+// let b = a;
+
+// b.push(4);
+
+// console.log(a); // output: [1,2,3,4] // "a" will also get changed because both of them i.e "a" & "b" are pointing at the same memory location:
+
+let a = [1, 2, 3];
+let b = [...a];
+
+b.push(4);
+
+console.log(a); // output: [1,2,3] // "a" will not get changed , because "b" is not pointing towards "a" , "b" has different memory location and "a" has different one.
+
