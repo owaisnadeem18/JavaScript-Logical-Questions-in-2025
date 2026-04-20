515857aac598fa9903db765a49c011111489006e
owaisnadeem18 <owaisnadeem15@gmail.com>
2026-04-20 15:25:52 +0500

    shallow copy vs deep copy

--- eOcean Logical JS questions Preparation/index.js
+++ eOcean Logical JS questions Preparation/index.js
@@ -45,7 +45,7 @@
 // console.log(a == b) // Output: false because both of them are stored at different memory locations
 
 // Reason: 
-// Spread Operator creates a new memory location for the new array and it copies all the elements of the original array into the new one. So, both "a" and "b" are stored at different memory locations and they are not equal to each other because “Objects/arrays are compared by reference, not by value. (shallow copy) 
+// Spread Operator creates a new memory location for the new array and it copies all the elements of the original array into the new one. So, both "a" and "b" are stored at different memory locations and they are not equal to each other because Objects/arrays are compared by reference, not by value. (shallow copy) 
 
 // let a = [1, 2, 3]
 
@@ -67,5 +67,27 @@
 
 // console.log(a); // output: [1,2,3] // "a" will not get changed , because "b" is not pointing towards "a" , "b" has different memory location and "a" has different one.
 
+// let a = [{x:1}];
+// let b = [...a];
 
-// 
+// b[0].x = 99;
+
+// console.log(a); // Output: {x:99} The outer array is copied, but the inner object is shared — that’s why the change is reflected in both.
+
+// 🔑 Golden Rule (Always Remember):
+
+// Shallow copy = Copies one level , all other things inside are shared. 
+
+const obj = {
+  name: "Owais",
+  address: {
+    city: "Karachi"
+  }
+};
+
+const copy = { ...obj };
+
+copy.address.city = "Lahore";
+
+console.log(obj.address.city); // Output: "Lahore" , This is because the spread operator creates a shallow copy of the object. It copies only the top-level properties, but the nested object (address) is still shared by reference. So, when we update copy.address.city, it also updates obj.address.city.
+
