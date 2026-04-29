e7282ce487811614684eeddbb625795ffa4e21d7
owaisnadeem18 <owaisnadeem15@gmail.com>
2026-04-29 06:39:37 +0500

    two sum problem

--- LinkitSoft Second Interview Round/logical_question.js
+++ LinkitSoft Second Interview Round/logical_question.js
@@ -18,33 +18,62 @@
 
 // -------------- Find the most frequent elements in the array ----------- 
 
-const string = "Owais Nadeem"
+// const string = "Owais Nadeem"
 
-const countOcc = (str) => {
+// const countOcc = (str) => {
+    
+//     const obj = {}
+//     let max_count = 0
+//     let most_freq_elem = []
+
+//     let trimmed_str = str.toLowerCase().split(" ").join("")
+
+//     for (let char of trimmed_str) {
+//         obj[char] = (obj[char] || 0) + 1
+        
+//         if (obj[char] > max_count) {
+//             max_count = obj[char]
+//             most_freq_elem = [char]
+//         } else if (obj[char] == max_count) {
+//             most_freq_elem.push(char)
+//         }
+
+//     }
+
+//     return [obj , most_freq_elem , max_count]
+
+// }
+
+// console.log(countOcc(string))
+
+// Two Sum (Without using extra space):
+
+// Find two numbers in an array that sum up to a target.
+
+// Input: [2, 7, 11, 15], target = 9
+// Output: [2, 7]
+
+const arr = [2, 7, 11, 15]
+let target = 9 
+
+const twoSum = (array) => {
     
     const obj = {}
-    let max_count = 0
-    let most_freq_elem = []
+    
+    for (let i = 0 ; i < array.length ; i++) {
+        let elem = array[i]
 
-    let trimmed_str = str.toLowerCase().split(" ").join("")
+        let complement = target - elem
 
-    for (let char of trimmed_str) {
-        obj[char] = (obj[char] || 0) + 1
-        
-        if (obj[char] > max_count) {
-            max_count = obj[char]
-            most_freq_elem = [char]
-        } else if (obj[char] == max_count) {
-            most_freq_elem.push(char)
+        if (obj[complement] !== undefined) {
+            return [obj[complement] , i]
         }
+
+        obj[elem] = i
 
     }
 
-    return [obj , most_freq_elem , max_count]
-
 }
 
-console.log(countOcc(string))
-
-
+console.log(twoSum(arr))
 
