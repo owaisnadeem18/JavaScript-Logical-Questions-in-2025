31efe21715a33177c422061db4f9887bfc8da452
owaisnadeem18 <owaisnadeem15@gmail.com>
2026-04-29 06:16:06 +0500

    count occ

---
+++ LinkitSoft Second Interview Round/logical_question.js
@@ -0,0 +1,17 @@
+// const string = "Owais Nadeem"
+
+// const countOcc = (str) => {
+    
+//     const obj = {}
+
+//     let trimmed_str = str.split(" ").join("")
+
+//     for (let char of trimmed_str) {
+//         obj[char] = (obj[char] || 0) + 1
+//     }
+
+//     return obj
+
+// }
+
+// console.log(countOcc(string))
